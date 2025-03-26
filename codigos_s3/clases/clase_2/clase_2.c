721ecb7469ac55e759b4b1d961625117cbed26d2
Seba <Sebastian141718floresflores@gmail.com>
2025-03-26 11:48:15 -0300

    :D

--- codigos_s3/clases/clase_2/clase_2.c
+++ codigos_s3/clases/clase_2/clase_2.c
@@ -29,29 +29,61 @@
 
 // INFO promedio v2
 
+// #include <stdio.h>
+//
+// int main() {
+//   int num, i,numeros[100];
+//
+//   // Funcion que calcula el promedio de una array de enteros
+//   float media (int n, int A[]) {
+//     int i, suma;
+//     suma = 0;
+//     for (i = 0; i < n; i++)
+//       suma += A[i];
+//     return (float) suma / n;
+//   }
+//
+//   // Ingreso de datos 
+//   printf("Cantidad de numeros: ");
+//   scanf("%d", &num);
+//   for (i = 0; i < num; i++) {
+//       printf("Ingresa el numero %d: ", i + 1);
+//       scanf("%i", &numeros[i]);
+//   }
+//
+//   // Se despliega el resultado
+//   printf("El promedio calculado es: %.2f\n", media(num,numeros));
+//   return 0;
+// }
+
+// INFO Registros en C
+
 #include <stdio.h>
+#include <string.h>
+
+struct Libro {
+  int idlibro;
+  char titulo[100];
+  char autor[100];
+  int fecha;
+};
 
 int main() {
-  int num, i,numeros[100];
+  // Se declara una variable de tipo libro
+  struct Libro libro1;
 
-  // Funcion que calcula el promedio de una array de enteros
-  float media (int n, int A[]) {
-    int i, suma;
-    suma = 0;
-    for (i = 0; i < n; i++)
-      suma += A[i];
-    return (float) suma / n;
-  }
+  // Se da valores a los campos de estructura
+  libro1.idlibro = 100221300;
+  strcpy(libro1.titulo, "Cien años de soledad");
+  strcpy(libro1.autor, "Gabriel García Márquez");
+  libro1.fecha = 1967;
 
-  // Ingreso de datos 
-  printf("Cantidad de numeros: ");
-  scanf("%d", &num);
-  for (i = 0; i < num; i++) {
-      printf("Ingresa el numero %d: ", i + 1);
-      scanf("%i", &numeros[i]);
-  }
+  // Impresion de la información del libro
+  printf("Identificacion : %d\n", libro1.idlibro);
+  printf("Título: %s\n", libro1.titulo);
+  printf("Autor : %s\n", libro1.autor);
+  printf("Fecha : %d\n", libro1.fecha);
 
-  // Se despliega el resultado
-  printf("El promedio calculado es: %.2f\n", media(num,numeros));
+  // Termino
   return 0;
 }
---
+++ codigos_s3/clases/clase_3/clase_3.c
@@ -0,0 +1,7 @@
+// INFO Problema propuesto: Completar el programa y ejecutarlo con valores de n entre 1 y 20. ¿Que pasa con valores superiores?
+
+// INFO Problemas con la solución propuesta: Esta propuesta funciona sólo con valores pequeños para n. Esto se debe al crecimiento explosivo del n!. Aunque compacta y elegante, se puede encontrar mejores soluciones para el factorial.
+
+// INFO Problema propuesto: Implemente esta misma versión recursiva en C o C++ y compare los resultados, llevando n al punto en que reciban una respuesta errónea.
+
+
