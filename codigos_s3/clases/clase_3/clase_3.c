8b1daad13c86e52b86d75ed17d156a2beafc2051
Seba <Sebastian141718floresflores@gmail.com>
2025-03-26 16:33:45 -0300

    :D

--- codigos_s3/clases/clase_3/clase_3.c
+++ codigos_s3/clases/clase_3/clase_3.c
@@ -1,6 +1,34 @@
 // INFO Problema propuesto: Completar el programa y ejecutarlo con valores de n entre 1 y 20. ¿Que pasa con valores superiores?
 
+#include <stdio.h>
+
+int main() {
+  int n, i;
+  unsigned long long factorial = 1; // Es un tipo de dato en C que permite almacenar numeros muy grandes solo positivos
+
+  printf("Escribe el número maximo:");
+  scanf("%d", &n);
+
+  if (n < 0) {
+    printf("El factorial no esta definido para numeros negativos ni cero.\n");
+  } else if ( n == 0 || n == 1) {
+    printf("El factorial de %d es 1. \n", n);
+  } else {
+    for (i = 2; i <= n; i++) {
+      factorial *= i;
+    }
+  printf("EL factorial de %d es %llu\n", n, factorial);
+  }
+
+  return 0;
+}
+
 // INFO Problemas con la solución propuesta: Esta propuesta funciona sólo con valores pequeños para n. Esto se debe al crecimiento explosivo del n!. Aunque compacta y elegante, se puede encontrar mejores soluciones para el factorial.
+
+
+
+
+
 
 // INFO Problema propuesto: Implemente esta misma versión recursiva en C o C++ y compare los resultados, llevando n al punto en que reciban una respuesta errónea.
 
