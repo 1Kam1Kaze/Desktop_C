9f4371282d681b2367488d9178339a7c8501ef1b
Seba <Sebastian141718floresflores@gmail.com>
2025-04-08 09:14:19 -0400

    :D

--- codigos_s3/clases/clase_1/clase_1.c
+++ codigos_s3/clases/clase_1/clase_1.c
@@ -2,7 +2,7 @@
 
 // INFO Estructura basica de un programa
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
 // int main() {
 //   printf("\nHello world!\n");
@@ -11,10 +11,10 @@
 
 //  INFO Estructura basica de un programa
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
 // int main() {
-//   double distancia, velocidad, tiempo;
+//   float distancia, velocidad, tiempo;
 //   velocidad = 120;
 //   tiempo = 2.5;
 //   distancia = velocidad * tiempo;
@@ -23,7 +23,7 @@
 
 // INFO Control de flujo
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
 // int main() {
 //   int suma;
@@ -33,7 +33,7 @@
 
 // INFO Repeticion (while)
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
 // int main() {
 //   int suma, númeroPar;
@@ -48,7 +48,7 @@
 
 // INFO Selecion (if)
 
-//#include <stdio.h> 
+// #include <stdio.h> 
 //
 // int main() {
 //   int número, suma;
@@ -71,15 +71,15 @@
 //     int número = 760;
 //     while (número <= 800 && número % 13 != 0)
 //         número++;
-//     if (número <= 800)
+//     if (número < 800)
 //         printf("%d", número);
 //     else
 //         printf("No se ha encontrado");
-// 
+// }
 
 // INFO Buscar primos en un intervalo
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
 // int main() {
 //   int inicio,fin;
@@ -100,37 +100,88 @@
 
 // INFO Buscando primos en un intervalo
 
-#include <stdio.h>
-
-int main() {
-    int inicio = 10, fin = 20;  // Establecemos el intervalo
-    int número, divisor;
-
-    // Bucle 'for' que recorre los números del intervalo [inicio, fin]
-    for (número = inicio; número <= fin; número++) {
-        printf("\n\n*** Comenzando con el número: %d ***\n", número);
-
-        divisor = 2;  // Iniciamos divisor desde 2
-
-        // Bucle 'while' que verifica si el número es divisible por algún divisor
-        while (número % divisor != 0 && divisor < número) {
-            printf("Divisor: %d, %d %% %d = %d\n", divisor, número, divisor, número % divisor);
-            divisor++;  // Incrementamos divisor
-        }
-
-        // Aquí verificamos si 'divisor' llegó a ser igual al número
-        if (divisor == número) {
-            printf("¡%d es primo! (divisor == número)\n", número);
-        } else {
-            printf("¡%d NO es primo! (divisor encontrado: %d)\n", número, divisor);
-        }
-    }
-}
+// #include <stdio.h>
+//
+// int main() {
+//     int inicio = 10, fin = 20;  // Establecemos el intervalo
+//     int número, divisor;
+//
+//     // Bucle 'for' que recorre los números del intervalo [inicio, fin]
+//     for (número = inicio; número <= fin; número++) {
+//         printf("\n\n*** Comenzando con el número: %d ***\n", número);
+//
+//         divisor = 2;  // Iniciamos divisor desde 2
+//
+//         // Bucle 'while' que verifica si el número es divisible por algún divisor
+//         while (número % divisor != 0 && divisor < número) {
+//             printf("Divisor: %d, %d %% %d = %d\n", divisor, número, divisor, número % divisor);
+//             divisor++;  // Incrementamos divisor
+//         }
+//
+//         // Aquí verificamos si 'divisor' llegó a ser igual al número
+//         if (divisor == número) {
+//             printf("¡%d es primo! (divisor == número)\n", número);
+//         } else {
+//             printf("¡%d NO es primo! (divisor encontrado: %d)\n", número, divisor);
+//         }
+//     }
+// }
 
 // Funciones
 
-//#include <stdio.h>
+// #include <stdio.h>
 //
+// int esPrimo(int números) {
+//   int divisor;
+//   divisor = 2;
+//   while (números % divisor != 0 && divisor < números) {
+//     divisor++;
+//   }
+//   return (divisor == números);
+// }
+//
+// int main() {
+//   int e,f;
+//   int números, divisor, esPrimo(int);
+//
+//   e = 10; f = 20;
+//   printf("Buscando entre los numeros %d y %d\n", e, f);
+//
+//   for (números = e; números <= f; números++) {
+//     if (esPrimo(números)) {
+//       printf("%d\n", números);
+//     }
+//   }
+// }
+
+// Funcion Void
+
+// #include <stdio.h>
+//
+// void saludar(char nombre[]) {
+//     printf("¡Hola, %s!\n", nombre);
+// }
+//
+// int main() {
+//     saludar("Juan"); // Llama a la función saludar. No se espera ningún valor de retorno.
+//     return 0;
+// }
+
+// Funcio void
+
+#include <stdio.h>
+
+void saludar(int numero) {
+    printf("¡Hola, %d!\n", numero);
+}
+
+int main() {
+    int numeros;
+    numeros = 3;
+    saludar(numeros);
+    return 0;
+}
+
 
 
 
---
+++ codigos_s3/clases/clase_4/clase_4.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+
+int main() {
+  float n;
+  printf("Escribeme una cantidad de numeross: ");
+  scanf("%f", &n);
+  printf("Tu numero es %.2f", n);
+  return 0;
+}
